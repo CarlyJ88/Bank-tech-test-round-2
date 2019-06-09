0c6c6345f0f190abfdee4c4ff4ac12078e4ef34a
Carly <carly.jenkinson65@googlemail.com>
2019-06-09 23:08:36 +0100

    Refactored statement

--- lib/account.rb
+++ lib/account.rb
@@ -7,18 +7,14 @@ class Account
 
   def statement
      header = "date || credit || debit || balance\n"
-     body = ''
-    if !@transaction.empty?
-      @transaction.each { |print_item| body += "#{print_item[:date].strftime("%d/%m/%Y")} || #{'%.2f' % print_item[:deposit]} || || #{'%.2f' % print_item[:balance]}\n" } 
-    end
-    header + body
-      
+     body = @transaction.map { |print_item| "#{print_item[:date].strftime("%d/%m/%Y")} || #{'%.2f' % print_item[:deposit]} || || #{'%.2f' % print_item[:balance]}\n" }.join
+     header + body 
   end
 
   def deposit(value)
-    @deposit = { date: Date.today, deposit: value, balance: @balance += value }
-    @transaction.unshift(@deposit)
-    @deposit
+    deposit = { date: Date.today, deposit: value, balance: @balance += value }
+    @transaction.unshift(deposit)
+    deposit
   end
 
   def transaction
